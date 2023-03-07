927a25da434be1dfb94fe74f39cca7a59bc6ef57
Richard Cranwell <rickycranwell@gmail.com>
2023-03-07 17:35:26 -0600

    followed tutorial to part 3

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+const mongoDB = 'mongodb+srv://book-master:xyzGh0st@express-library-page.8wjxbti.mongodb.net/local_library?retryWrites=true&w=majority';
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
