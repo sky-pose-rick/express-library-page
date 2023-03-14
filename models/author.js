083997a05c6f0f1df0279d33392b6c4666c7fd9e
Richard Cranwell <rickycranwell@gmail.com>
2023-03-14 17:41:41 -0500

    Follow tutorial to part 5

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {DateTime} = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -28,6 +29,17 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+// virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(function (){
+  if(!this.date_of_birth && !this.date_of_death)
+    return 'Uknown Lifespan';
+
+  const birthFormatted = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  const deathFormatted = this.date_of_death? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+
+  return `${birthFormatted} - ${deathFormatted}`;
+})
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
